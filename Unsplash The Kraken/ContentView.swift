6080364b4c3bd9fd0f5f553c58cc185d0295a48a
rasyidrw <rasyidrespati.30@gmail.com>
2020-06-18 19:56:05 +0700

    Initial Commit

---
+++ Unsplash The Kraken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unsplash The Kraken
+//
+//  Created by Rasyid Respati Wiriaatmaja on 18/06/20.
+//  Copyright © 2020 rasyidrw. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
